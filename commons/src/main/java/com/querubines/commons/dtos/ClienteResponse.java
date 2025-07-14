94d93340f680ad2cd17fac11de19df6eeb0cb5c9
Diana Hernandez Olguin <dianis03hdezolg@gmail.com>
2025-07-14 14:48:26 -0600

    ADD: Se agrega request Cliente

--- commons/src/main/java/com/querubines/commons/dtos/ClienteResponse.java
+++ commons/src/main/java/com/querubines/commons/dtos/ClienteResponse.java
@@ -2,7 +2,11 @@ package com.querubines.commons.dtos;
 
 public record ClienteResponse(
 		
-		
+		String nombre,
+		String apellido,
+		String email,
+		int telefono,
+		String direccion
 		
 		) {
 
