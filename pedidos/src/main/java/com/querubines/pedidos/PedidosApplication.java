67d2373371a5a07e850d4140c6838c44bcab46b2
jugodev2 <pruebashugoalonso@gmail.com>
2025-07-15 08:12:40 -0600

    ADD
    Se agregan funcionalidades a Pedidos

--- pedidos/src/main/java/com/querubines/pedidos/PedidosApplication.java
+++ pedidos/src/main/java/com/querubines/pedidos/PedidosApplication.java
@@ -2,8 +2,12 @@ package com.querubines.pedidos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.querubines.commons.controller", "com.querubines.pedidos"})
+@EntityScan({"com.querubines.commons.models.entities"})
+@EnableFeignClients
 public class PedidosApplication {
 
 	public static void main(String[] args) {
---
+++ pedidos/src/main/java/com/querubines/pedidos/clients/ProductoClient.java
@@ -0,0 +1,15 @@
+package com.querubines.pedidos.clients;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.querubines.commons.dtos.ProductoResponse;
+
+@FeignClient(name = "productos")
+public interface ProductoClient {
+	
+	@GetMapping("/{id}")
+	ProductoResponse getProducto(@PathVariable Long id);
+
+}
