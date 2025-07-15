25491f1d0113ef70b2ee19b744a76511dbc8ab14
Diana Olguin <dianis03hdezolg@gmail.com>
2025-07-14 23:15:42 -0600

    ADD
    actualizacion de los dto pedido y entitis de pedido

--- commons/src/main/java/com/querubines/commons/dtos/PedidoResponse.java
+++ commons/src/main/java/com/querubines/commons/dtos/PedidoResponse.java
@@ -2,8 +2,9 @@ package com.querubines.commons.dtos;
 
 public record PedidoResponse(
 		
+		Long id, 
+		String nombre,
+		Double precio,
+		int stock
 		
-		
-		) {
-
-}
+		) {}
--- commons/src/main/java/com/querubines/commons/models/entities/Producto.java
+++ commons/src/main/java/com/querubines/commons/models/entities/Producto.java
@@ -30,4 +30,53 @@ public class Producto {
 	@Column (name = "STOCK", nullable = false, unique = true)
 	private int stock;
 
+	
+	
+	public Producto() {
+		super();
+	}
+	
+	
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public Double getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(Double precio) {
+		this.precio = precio;
+	}
+
+	public int getStock() {
+		return stock;
+	}
+
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
+
+	
 }
