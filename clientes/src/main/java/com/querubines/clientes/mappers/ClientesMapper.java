97b6277c054b8ad0171dc883f97fe1dd70dc24b0
Alfredo Garcia <alfredog95r@gmail.com>
2025-07-15 02:25:38 -0600

    ADD: Servicio Cliente terminado

--- clientes/src/main/java/com/querubines/clientes/mappers/ClientesMapper.java
+++ clientes/src/main/java/com/querubines/clientes/mappers/ClientesMapper.java
@@ -1,28 +1,48 @@
 package com.querubines.clientes.mappers;
 
+import org.springframework.stereotype.Component;
+
 import com.querubines.commons.dtos.ClienteRequest;
 import com.querubines.commons.dtos.ClienteResponse;
 import com.querubines.commons.mappers.CommonMapper;
 import com.querubines.commons.models.entities.Cliente;
 
+@Component
 public class ClientesMapper extends CommonMapper<ClienteRequest, ClienteResponse, Cliente> {
+	
+	
 
 	@Override
-	protected ClienteResponse entityToResponse(Cliente entity) {
+	public ClienteResponse entityToResponse(Cliente entity) {
 		if (entity == null) {
 			return null;
 		}
 		return new ClienteResponse(
-				
+				entity.getId(),
+				entity.getNombre(),
+				entity.getApellido(),
+				entity.getEmail(),
+				entity.getTelefono(),
+				entity.getDireccion()
 		);
+				
+		
 	}
 
 	@Override
-	protected Cliente requestToEntity(ClienteRequest request) {
-		// TODO Auto-generated method stub
-		return null;
+	public Cliente requestToEntity(ClienteRequest request) {
+		if (request == null) {
+			return null;
+		}
+		Cliente cliente = new Cliente();
+		cliente.setNombre(request.nombre());
+		cliente.setApellido(request.apellido());
+		cliente.setEmail(request.email());
+		cliente.setTelefono(request.telefono());
+		cliente.setDireccion(request.direccion());
+		return cliente;
 	}
-	
+
 	
 }
 
